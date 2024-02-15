3439e816694e47539160b3c7c21f2a71739e7ff7
samujon <samuel.veddige@gmail.com>
2024-02-15 14:26:46 +0100

    start refactoring

--- src/VGG16_refactored.cpp
+++ src/VGG16_refactored.cpp
@@ -5,17 +5,88 @@
 #include <unordered_map>
 
 #include "oneapi/dnnl/dnnl.hpp"
+//#include "VGG16_helpers.hpp"
 #include "/home/samjons/thesis/oneDNN/examples/example_utils.hpp"
 
 using namespace dnnl;
 
+// Helper function to create and initialize a convolution layer
+std::tuple<primitive, std::unordered_map<int, memory>> create_convolution_layer(
+    engine& eng, stream& s, memory& src, const memory::dims& src_tz, const memory::dims& weights_tz,
+    const memory::dims& bias_tz, const memory::dims& dst_tz, const memory::dims& strides,
+    const memory::dims& padding, float* weights_data, float* bias_data) {
+    
+    // Memory for weights and bias
+    auto user_weights_memory = memory({{weights_tz}, memory::data_type::f32, memory::format_tag::oihw}, eng, weights_data);
+    auto user_bias_memory = memory({{bias_tz}, memory::data_type::f32, memory::format_tag::x}, eng, bias_data);
+    
+    // Write data to memory
+    write_to_dnnl_memory(weights_data, user_weights_memory);
+    write_to_dnnl_memory(bias_data, user_bias_memory);
+
+    // Descriptors
+    auto src_md = memory::desc({src_tz}, memory::data_type::f32, memory::format_tag::any);
+    auto weights_md = memory::desc({weights_tz}, memory::data_type::f32, memory::format_tag::any);
+    auto bias_md = memory::desc({bias_tz}, memory::data_type::f32, memory::format_tag::any);
+    auto dst_md = memory::desc({dst_tz}, memory::data_type::f32, memory::format_tag::any);
+    
+    // Convolution descriptor
+    auto conv_desc = convolution_forward::desc(prop_kind::forward_inference,
+        algorithm::convolution_direct, src_md, weights_md, bias_md, dst_md,
+        strides, padding, padding);
+    
+    auto conv_pd = convolution_forward::primitive_desc(conv_desc, eng);
+    
+    // Reorder source memory if necessary
+    auto conv_src = src;
+    if (conv_pd.src_desc() != src.get_desc()) {
+        conv_src = memory(conv_pd.src_desc(), eng);
+        reorder(src, conv_src).execute(s, src, conv_src);
+    }
+    
+    // Reorder weights memory if necessary
+    auto conv_weights = user_weights_memory;
+    if (conv_pd.weights_desc() != user_weights_memory.get_desc()) {
+        conv_weights = memory(conv_pd.weights_desc(), eng);
+        reorder(user_weights_memory, conv_weights).execute(s, user_weights_memory, conv_weights);
+    }
+    
+    // Output memory
+    auto conv_dst = memory(conv_pd.dst_desc(), eng);
+    
+    // Prepare arguments for the convolution primitive
+    std::unordered_map<int, memory> conv_args = {
+        {DNNL_ARG_SRC, conv_src},
+        {DNNL_ARG_WEIGHTS, conv_weights},
+        {DNNL_ARG_BIAS, user_bias_memory},
+        {DNNL_ARG_DST, conv_dst}
+    };
+    
+    return {convolution_forward(conv_pd), conv_args};
+}
+
+// Helper function to execute an activation layer (e.g., ReLU)
+std::tuple<primitive, std::unordered_map<int, memory>> create_activation_layer(
+    engine& eng, const memory& src, float slope) {
+    
+    // Activation descriptor
+    auto relu_desc = eltwise_forward::desc(prop_kind::forward_inference,
+        algorithm::eltwise_relu, src.get_desc(), slope);
+    auto relu_pd = eltwise_forward::primitive_desc(relu_desc, eng);
+    
+    // Arguments for the ReLU primitive
+    std::unordered_map<int, memory> relu_args = {
+        {DNNL_ARG_SRC, src},
+        {DNNL_ARG_DST, src} // In-place operation
+    };
+    
+    return {eltwise_forward(relu_pd), relu_args};
+}
 // CPU engine implementation
 
 // In comparison with AlexNet, VGG16 does not LRN, local response normalization
 // VGG16 D configuration
 // A refactored version of VGG16.cpp
-
-
 void VGG16(engine::kind engine_kind){
         std::cout << "Entered VGG16" << std::endl;
         using tag = memory::format_tag;
@@ -112,7 +183,7 @@ void VGG16(engine::kind engine_kind){
         // ReLu1
         std::cout << "ReLu1" << std::endl;
         const float negative1_slope = 0.0f;
-
+        /*
         // Create ReLu primitive
         auto relu1_desc = eltwise_forward::desc(prop_kind::forward_inference,
         algorithm::eltwise_relu, conv1_dst_memory.get_desc(),
@@ -122,6 +193,11 @@ void VGG16(engine::kind engine_kind){
         net.push_back(eltwise_forward(relu1_prim_desc));
         net_args.push_back({{DNNL_ARG_SRC, conv1_dst_memory},
         {DNNL_ARG_DST, conv1_dst_memory}});
+        */
+        std::tuple<primitive, std::unordered_map<int, memory>> relu1 = create_activation_layer(eng, conv1_dst_memory, negative1_slope);
+        net.push_back(std::get<0>(relu1));
+        net_args.push_back(std::get<1>(relu1));
+
 
         // -----------------------------------------------------------
         // convolutional layer 2: 224x224x64
